6e0807cc42e1e899aa75febaee31ebbf0bd884ca
Youngjun Jang <105337194+yjjang1128@users.noreply.github.com>
2023-08-29 19:02:39 +0900

    Add function at Hangman.py
    1. Life function
    2. Guess word at once

--- Hangman.py
+++ Hangman.py
@@ -1,24 +1,42 @@
 import random
 words = ['dog', 'chicken', 'mouse']
+
 def game_start():
     # Choice a random word from 'words'.
     word = list(random.choice(words))
     answer = ['_'] * len(word)
+    life = 6
+    # Number of misspelled words.
+    misspell = answer.count('_')
 
     # Loop that does not end until the 'word' is matched.
     while word != answer:
         # Input the word you guess.
-        char = input("input character > ")
+        char = input("Input character >>> ")
 
         # Loop through the list and check if 'char' is equal.
         for i in range(len(word)):
             if word[i] == char:
                 answer[i] = char
+            # Guess word at once.
+            elif "".join(word) == char:
+                return print("Correct! The answer was \"{}\"!".format("".join(word)))
             else:
                 continue
+        
+        # Compare present 'answer' and past 'answer' to change 'life'.
+        if misspell == answer.count('_'):
+            life -= 1
+            # Condition of gameover.
+            if life == 0:
+                return print("Game Over")
+            print("That character does not exist in the word.")
+        else:
+            misspell = answer.count('_')
+
 
         # Show current status.
-        print(" ".join(answer))
+        print(" ".join(answer), "       Life: {}".format(life))
         print()
 
     # Show you are correct.
