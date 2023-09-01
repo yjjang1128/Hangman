6fa41f03c79ceaca04dc20f58b35d8e4806d1edb
Youngjun Jang <105337194+yjjang1128@users.noreply.github.com>
2023-09-01 19:02:55 +0900

    Add ASCII Art
    Added ASCII Art of "Hangman", "Correct" and "Game over"

--- Hangman.py
+++ Hangman.py
@@ -8,38 +8,119 @@ def game_start():
     life = 6
     # Number of misspelled words.
     misspell = answer.count('_')
+    # List of current status.
+    img = ["""
+   LIFE: {}
+   ________
+   |      |
+   O      |
+  /|\\     |
+  / \\     |
+          |
+         ---
+   {}
+    """,
+    """
+   LIFE: {}
+   ________
+   |      |
+   O      |
+  /|\\     |
+  /       |
+          |
+         ---
+   {}
+    """,
+    """
+   LIFE: {}
+   ________
+   |      |
+   O      |
+  /|\\     |
+          |
+          |
+         ---
+   {}
+    """,
+    """
+   LIFE: {}
+   ________
+   |      |
+   O      |
+  /|      |
+          |
+          |
+         ---
+   {}
+    """,
+    """
+   LIFE: {}
+   ________
+   |      |
+   O      |
+   |      |
+          |
+          |
+         ---
+   {}
+    """,
+    """
+   LIFE: {}
+   ________
+   |      |
+   O      |
+          |
+          |
+          |
+         ---
+   {}
+    """]
 
     # Loop that does not end until the 'word' is matched.
     while word != answer:
+        # Show current status.
+        print(img[6 - life].format(life, " ".join(answer)))
         # Input the word you guess.
         char = input("Input character >>> ")
+
+        # Guess word at once.
+        if char == "".join(word):
+            return print("""
+   |￣￣￣￣￣￣￣￣￣￣￣￣|
+           Correct!
+        The answer was
+         >>> {} <<<
+   |＿＿＿＿＿＿＿＿＿＿＿＿|
+    """.format("".join(word)))
 
         # Loop through the list and check if 'char' is equal.
         for i in range(len(word)):
             if word[i] == char:
                 answer[i] = char
-            # Guess word at once.
-            elif "".join(word) == char:
-                return print("Correct! The answer was \"{}\"!".format("".join(word)))
             else:
                 continue
-        
+
         # Compare present 'answer' and past 'answer' to change 'life'.
         if misspell == answer.count('_'):
             life -= 1
             # Condition of gameover.
             if life == 0:
-                return print("Game Over")
-            print("That character does not exist in the word.")
+                return print("""
+   |￣￣￣￣￣￣￣￣￣|
+        Game Over
+   |＿＿＿＿＿＿＿＿＿|""")
+            print()
+            print("\"{}\" does not exist in the word.".format(char))
         else:
             misspell = answer.count('_')
 
-
-        # Show current status.
-        print(" ".join(answer), "       Life: {}".format(life))
-        print()
-
     # Show you are correct.
-    return print("Correct! The answer was \"{}\"!".format("".join(word)))
+    return print("""
+   |￣￣￣￣￣￣￣￣￣￣￣￣|
+           Correct!
+        The answer was
+        >>> {} <<<
+   |＿＿＿＿＿＿＿＿＿＿＿＿|
+    """.format("".join(answer)))
 
 game_start()
